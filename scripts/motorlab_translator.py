8e84d3989f1165457e0551b4fed616bc06a8fe0b
Benjamin Kuo <benjamik@andrew.cmu.edu>
2016-10-10 21:41:04 -0400

    added a few translation fuctions to convert to actual units

--- scripts/motorlab_translator.py
+++ scripts/motorlab_translator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from motorlab_msgs.msg import MotorLab_Arduino
 from motorlab_msgs.msg import MotorLab_Arduino_Translation
 
@@ -21,11 +22,38 @@ class Arduino_Translator(object):
 		self.Arduino_translation.dc_motor_speed = self.Arduino_result.dc_motor_speed
 		self.Arduino_translation.servo_position = self.Arduino_result.servo_position
 		self.Arduino_translation.stepper_motor_position = self.Arduino_result.stepper_motor_position
-		self.Arduino_translation.temperature = self.Arduino_result.temperature
+		self.Arduino_translation.temperature = self.translate_temp(self.Arduino_result.temperature)
 		self.Arduino_translation.light_gate_state = self.Arduino_result.light_gate_state
 		self.Arduino_translation.ultrasonic_distance = self.Arduino_result.ultrasonic_distance
-		self.Arduino_translation.ir_distance = self.Arduino_result.ir_distance
+		self.Arduino_translation.ir_distance = self.translate_IR(self.Arduino_result.ir_distance)
 		self.arduino_pub.publish(self.Arduino_translation)
+
+	def translate_temp(self,rawADC):
+		Temp = 0.0
+		if rawADC > 0:
+			# Temp = math.log(10000.0*((1024.0/rawADC-1))); 
+			# Temp = math.log(10000.0/(1024.0/rawADC-1)) # for pull-up configuration
+			# Temp = 1.0 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+
+			# Temp = Temp - 273.15;            # Convert Kelvin to Celcius
+			# Temp = (Temp * 9.0)/ 5.0 + 32.0; # Convert Celcius to Fahrenheit
+			Temp = 0.0968*rawADC-22.943
+			Temp = (Temp * 9.0)/ 5.0 + 32.0; # Convert Celcius to Fahrenheit
+		else:
+			Temp = -300
+		return Temp;
+
+	def translate_IR(self,rawADC):
+		dist = -0.072*rawADC+41.531
+		if dist > 40.0:
+			return 40.0
+		return dist
+
+	def translate_ultra(self, rawADC):
+		dist = rawADC*(5.0/1023.0)/(5.0/512.0) #inches
+		if dist > 40.0:
+			return 40.0
+		return dist
 
 if __name__ == '__main__':
 
